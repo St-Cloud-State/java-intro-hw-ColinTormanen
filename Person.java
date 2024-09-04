72e6d96f9448a8e4230ec8f3dd00e3e6bba22caa
ColinTormanen <144750228+ColinTormanen@users.noreply.github.com>
2024-09-04 20:16:45 +0000

    9/4/24

--- Person.java
+++ Person.java
@@ -1,9 +1,16 @@
-
+// File for the class Person
 
 public class Person {
     private String firstName;
     private String lastName;
     private int id; 
+
+    // Person constructor
+    public Person(String fname, String lname, int i) {
+        firstName = fname;
+        lastName = lname;
+        id = i;
+    }
 
     // First name setter
     public void setFirstName(String name) {
@@ -25,11 +32,19 @@ public class Person {
         return lastName;
     }
 
+    // Person id setter
     public void setId(int i) {
         id = i;
     }
 
+    // Person id getter
     public int getId() {
         return id;
+    }
+
+    // Returns the object as a complete string
+    @Override
+    public String toString() {
+        return "First name: " + firstName  + " Last name: " + lastName + " Id: " + id;
     }
 }
